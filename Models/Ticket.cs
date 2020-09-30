f9a984fc0d8c68a09df97df9d2ad3bcbf9131b8d
Vanolivano <gerasim.v@list.ru>
2020-09-30 15:33:15 +0300

    add validation

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace CheckMyTicket.Models
 {
     public class Ticket
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Укажите тираж билета")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "Недопустимая длина тиража билета")]
         public string Edition { get; set; }
+        [Required(ErrorMessage = "Укажите номер билета")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "Недопустимая длина номера билета")]
         public string Number { get; set; }
     }
 }
