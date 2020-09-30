8ce250d5a4b61af05737ace17a138eed5b68d447
Vanolivano <gerasim.v@list.ru>
2020-09-30 17:01:45 +0300

    add tests

--- CheckMyTicket.Tests/PrimeService_IsPrimeShould.cs
+++ CheckMyTicket.Tests/PrimeService_IsPrimeShould.cs
@@ -1,36 +1,62 @@
 using System.Threading.Tasks;
 using CheckMyTicket.Controllers;
 using Xunit;
-using System.Text.Json;
-using Moq;
 using CheckMyTicket.Models;
-using CheckMyTicket.Test;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CheckMyTicket.Tests
 {
     public class CheckMyTicket_IsWin
     {
-        private readonly TicketsController _ticketsController;
 
         public CheckMyTicket_IsWin()
         {
-            var mock = new Mock<TicketContext>();
-            mock.Setup(x => x.Set<Ticket>())
-                .Returns(new FakeDbSet<Ticket>
-                {
-                    new Ticket {Id = 1, Edition = "123", Number = "A234D31"},
-                    new Ticket {Id = 2, Edition = "123", Number = "A234D32"},
-                    new Ticket {Id = 3, Edition = "123", Number = "A234D33"}
-                });
-            _ticketsController = new TicketsController(mock.Object);
+           
+           
         }
 
         [Fact]
-        public async Task CheckMyTicket_InputIs1_ReturnFalse()
+        public async Task CheckMyTicket_Input_ReturnFalse()
         {
-            var result = await _ticketsController.CheckMyTicket(new Models.Ticket{ Edition = "111", Number = "1234567"});
-            var isResult = JsonSerializer.Deserialize<bool>(result.ToString());
-            Assert.False(isResult, "1 should not be prime");
+            //Arrange - create In Memory Database
+            var options = new DbContextOptionsBuilder<TicketContext>().UseInMemoryDatabase(databaseName: "TicketsDataBase").Options;
+            //Create mocked Context
+            using (var context = new TicketContext(options))
+            {
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D31"});
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D32"});
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D33"});
+                context.SaveChanges();
+            }
+            using (var context = new TicketContext(options))
+            {
+                var ticketsController = new TicketsController(context);
+                var result = await ticketsController.CheckMyTicket(new Models.Ticket{ Edition = "111", Number = "1234567"}) as ObjectResult;
+                var actualResult = result.Value;
+                Assert.False((bool)actualResult, "Tiket 111-1234567 should not be win");
+            }
+        }
+        [Fact]
+        public async Task CheckMyTicket_Input_ReturnTrue()
+        {
+            //Arrange - create In Memory Database
+            var options = new DbContextOptionsBuilder<TicketContext>().UseInMemoryDatabase(databaseName: "TicketsDataBase").Options;
+            //Create mocked Context
+            using (var context = new TicketContext(options))
+            {
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D31"});
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D32"});
+                context.Tickets.Add(new Ticket {Edition = "123", Number = "A234D33"});
+                context.SaveChanges();
+            }
+            using (var context = new TicketContext(options))
+            {
+                var ticketsController = new TicketsController(context);
+                var result = await ticketsController.CheckMyTicket(new Models.Ticket{ Edition = "123", Number = "A234D31"}) as ObjectResult;
+                var actualResult = result.Value;
+                Assert.True((bool)actualResult, "Tiket 123-A234D31 should be win");
+            }
         }
     }
 }
